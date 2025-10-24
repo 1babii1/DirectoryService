1972f410bef7693855f6a39c5b7f852c52768978
babii <sabangulov.ilnaz@mail.ru>
2025-10-24 19:25:25 +0500

    test

---
+++ DirectoryService.Domain/DepartmentPositions/ValueObjects/DepartmentPositionsId.cs
@@ -0,0 +1,17 @@
+﻿namespace DirectoryService.Domain.DepartmentPositions.ValueObjects;
+
+public record DepartmentPositionsId
+{
+    public Guid Value { get; }
+
+    private DepartmentPositionsId(Guid value)
+    {
+        Value = value;
+    }
+
+    public static DepartmentPositionsId NewDepartmentPositionsId() => new(Guid.NewGuid());
+
+    public static DepartmentPositionsId FromValue(Guid value) => new(value);
+
+    public static DepartmentPositionsId Empty() => new(Guid.Empty);
+}
---
+++ DirectoryService.Domain/Positions/ValueObjects/PositionDescription.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json.Nodes;
+using CSharpFunctionalExtensions;
+using Shared;
+
+namespace DirectoryService.Domain.Positions.ValueObjects;
+
+public record PositionDescription
+{
+    public string Value { get; init; }
+
+    private PositionDescription(string value)
+    {
+        Value = value;
+    }
+
+    public static Result<PositionDescription, Error> Create(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            return Error.Validation(null, "Position description cannot be empty");
+
+        string trimmed = value.Trim();
+
+        if (trimmed.Length >= LenghtConstants.LENGTH1000)
+            return Error.Validation("length.is.invalid", "Position description must be between 3 and 100 characters");
+
+        PositionDescription description = new(trimmed);
+
+        return Result.Success<PositionDescription, Error>(description);
+    }
+}

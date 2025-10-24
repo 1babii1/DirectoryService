5d9d85498a8da0208ee6b99f1e97a6e90409c67d
babii <sabangulov.ilnaz@mail.ru>
2025-10-24 18:32:25 +0500

    Domain model done

---
+++ DirectoryService.Domain/Locations/ValueObjects/Timezone.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+using CSharpFunctionalExtensions;
+using Shared;
+
+namespace DirectoryService.Domain.Locations.ValueObjects;
+
+public partial record Timezone
+{
+    public string Value { get; init; }
+
+    private Timezone(string value)
+    {
+        Value = value;
+    }
+
+    public static Result<Timezone, Error> Create(string value)
+    {
+        if(string.IsNullOrWhiteSpace(value))
+            return Error.Validation(null, "Timezone is required");
+
+        if(!TimezoneRegex().IsMatch(value))
+            return Error.Validation(null, "Timezone is invalid");
+
+        Timezone timezone = new(value);
+
+        return Result.Success<Timezone, Error>(timezone);
+    }
+
+    [GeneratedRegex(@"^[A-Za-z_]+\/[A-Za-z_]+$")]
+    private static partial Regex TimezoneRegex();
+}
